efe3036df208cd71d203991143c2987672ac2025
Wang-Junao <1018080220@qq.com>
2023-03-18 08:12:32 +0800

    //

--- 1.cpp
+++ 1.cpp
@@ -10,10 +10,16 @@ public:
 bool operator>(const HighSub& a,const HighSub& b) {
     int len1=a.num.size();
     int len2=b.num.size();
+    string inserter="0";
     if(len1<len2) {
         for(int i=0;i<len2-len1;i++) {
             cout<<a.num<<endl;
-            a.num=
+            a.num=a.num.insert(0,"0");
+        }
+    }
+    else {
+        for(int i=0;i<len1-len2;i++) {
+            b.num=
         }
     }
 }
