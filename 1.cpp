0a7f339c791b4364e75e77493c4517e53b4dc975
Wang-Junao <1018080220@qq.com>
2023-03-18 08:22:06 +0800

    //第一题

--- 1.cpp
+++ 1.cpp
@@ -3,27 +3,21 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-class HighSub {
-public:
-    string num;
-};
-bool operator>(const HighSub& a,const HighSub& b) {
-    int len1=a.num.size();
-    int len2=b.num.size();
-    string inserter="0";
+int main() {
+    string a,b;
+    cin >>a>>b;
+    int len1=a.size();
+    int len2=b.size();
     if(len1<len2) {
         for(int i=0;i<len2-len1;i++) {
-            cout<<a.num<<endl;
-            a.num=a.num.insert(0,"0");
+            a.insert(0,"0");
         }
     }
     else {
         for(int i=0;i<len1-len2;i++) {
-            b.num=
+            b.insert(0,"0");
         }
     }
-}
-int main() {
-
+    cout<<a<<"\n"<<b<<endl;
     return 0;
 }
