53115c45de871a23a49a1f0de1b7f0108fa0fe01
Wang-Junao <1018080220@qq.com>
2023-03-18 08:37:36 +0800

    //

--- 1.cpp
+++ 1.cpp
@@ -2,30 +2,29 @@
 // Created by Junhao on 2023/3/18.
 //
 #include <bits/stdc++.h>
+
 using namespace std;
+
 int main() {
-    string a,b;
-    cin >>a>>b;
-    int len1=a.size();
-    int len2=b.size();
-    if(len1<len2) {
-        for(int i=0;i<len2-len1;i++) {
-            a.insert(0,"0");
+    string a, b;
+    cin >> a >> b;
+    int len1 = a.size();
+    int len2 = b.size();
+    if (len1 < len2) {
+        for (int i = 0; i < len2 - len1; i++) {
+            a.insert(0, "0");
+        }
+    } else {
+        for (int i = 0; i < len1 - len2; i++) {
+            b.insert(0, "0");
         }
     }
-    else {
-        for(int i=0;i<len1-len2;i++) {
-            b.insert(0,"0");
-        }
-    }
-    if(a>b) {
-        cout<<"greater"<<endl;
-    }
-    else if(a<b) {
-        cout<<"less"<<endl;
-    }
-    else {
-        cout<<"equal"<<endl;
+    if (a > b) {
+        cout << "greater" << endl;
+    } else if (a < b) {
+        cout << "less" << endl;
+    } else {
+        cout << "equal" << endl;
     }
     return 0;
 }
--- 2.cpp
+++ 2.cpp
@@ -3,24 +3,71 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-const int N=1010;
-int a[N],b[N],c[N];
-void invertElem(char s[], size_t n)
+const int N = 1010;
+int a[N], b[N], result[N];
+string s1, s2;
+int lena, lenb, lenmax;
+bool fu = false;
+bool checkzf()
 {
-    size_t len = n-1;
-    for(size_t i=0,j=len;i<j;i++,j--)
+    int flag = 0;
+    if(lena > lenb) return true;
+    if(lena < lenb) return false;
+    if(lena == lenb)
     {
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
+        for(int i = lena - 1; i >= 0; i--)
+        {
+            if(a[i] > b[i])
+            {
+                flag = 1;
+                return true;
+            }
+            if(a[i] < b[i])
+            {
+                flag = 1;
+                return false;
+            }
+        }
     }
+    if(flag == 0) return true;
 }
-void charInt(char s[], size_t n)
+int main()
 {
-    for(size_t i=0; i<n; i++)
-        s[i]-=48;
-}
-int main() {
-
+    cin >> s1 >> s2;
+    lena = s1.size();
+    lenb = s2.size();
+    lenmax = max(lena, lenb);
+    //倒序存储
+    for(int i = 0; i < lena; i++) a[i] = s1[lena - i - 1] - '0';
+    for(int i = 0; i < lenb; i++) b[i] = s2[lenb - i - 1] - '0';
+    if(checkzf())
+    {
+        for(int i = 0; i < lena; i++)
+        {
+            if(a[i] < b[i])
+            {
+                a[i + 1]--;
+                a[i] += 10;
+            }
+            result[i] = a[i] - b[i];
+        }
+    }
+    else
+    {
+        fu = true;
+        for(int i = 0; i < lenmax; i++)
+        {
+            if(b[i] < a[i])
+            {
+                b[i + 1] -= 1;
+                b[i] += 10;
+            }
+            result[i] = b[i] - a[i];
+        }
+    }
+    int tmp = lenmax;
+    while(result[tmp] == 0 && tmp > 0) tmp--;
+    if(fu) printf("-");
+    for(int i = tmp; i >= 0; i--) cout << result[i];
     return 0;
 }
