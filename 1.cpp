8d846a3e0e846027fb8abd18a778fd02ee611bfe
Wang-Junao <1018080220@qq.com>
2023-03-18 08:04:10 +0800

    //

--- 1.cpp
+++ 1.cpp
@@ -3,6 +3,20 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
+class HighSub {
+public:
+    string num;
+};
+bool operator>(const HighSub& a,const HighSub& b) {
+    int len1=a.num.size();
+    int len2=b.num.size();
+    if(len1<len2) {
+        for(int i=0;i<len2-len1;i++) {
+            cout<<a.num<<endl;
+            a.num=
+        }
+    }
+}
 int main() {
 
     return 0;
