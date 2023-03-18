faad1a39c1623b44bda7e43ce04daa7a7e34876d
Wang-Junao <1018080220@qq.com>
2023-03-18 09:11:57 +0800

    //

--- 3.cpp
+++ 3.cpp
@@ -1,3 +1,23 @@
 //
 // Created by Junhao on 2023/3/18.
 //
+#include <bits/stdc++.h>
+using namespace std;
+string xx;
+stack<char>xs;
+string flag="not point";
+int main() {
+    cin>>xx;
+    int len=xx.size();
+    for(int i=0;i<len;i++) {
+        if(xx[i]=='.') {
+            flag="point";
+        }
+        if(xx[i]!='0'&&flag=="not point") {
+            cout<<xx[i];
+        }
+    }
+    cout<<".";
+
+    return 0;
+}
